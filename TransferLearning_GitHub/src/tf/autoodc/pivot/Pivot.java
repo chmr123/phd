7e9ff37c4e45b3dddcbb3811db5bd1aebd3d19cb
Mingrui Chen <mingruic@smu.edu>
2015-11-12 08:47:11 -0600

    update pivot

--- TransferLearning_GitHub/src/tf/autoodc/pivot/Pivot.java
+++ TransferLearning_GitHub/src/tf/autoodc/pivot/Pivot.java
@@ -1,5 +1,13 @@
 package tf.autoodc.pivot;
 
-public class Pivot {
+import java.util.Map;
 
+public class Pivot {
+	static int window_size = 3;
+	
+	public void getPivotFeature(Map<String, String[]> alltext, Map<String[], String> keywords_map){
+		for(String text : alltext.keySet()){
+			
+		}
+	}
 }
