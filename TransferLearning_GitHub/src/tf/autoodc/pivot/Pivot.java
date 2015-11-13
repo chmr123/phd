74db5fb2edbf12c1d9a82382fc5094970e35d8c6
mingrui@Expressway Tower <mingruic@smu.edu>
2015-11-13 02:33:39 -0600

    pivot updated at tower

--- TransferLearning_GitHub/src/tf/autoodc/pivot/Pivot.java
+++ TransferLearning_GitHub/src/tf/autoodc/pivot/Pivot.java
@@ -4,9 +4,9 @@ import java.util.Map;
 
 public class Pivot {
 	static int window_size = 3;
-	
-	public void getPivotFeature(Map<String, String[]> alltext, Map<String[], String> keywords_map){
-		for(String text : alltext.keySet()){
+
+	public void getPivotFeature(Map<String, String[]> alltext, Map<String[], String> keywords_map) {
+		for (String text : alltext.keySet()) {
 			
 		}
 	}
