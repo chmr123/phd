cb1cd97226f59555a6a14c15dd9e2a33df5b932a
mingrui <mingruic@smu.edu>
2015-11-13 12:28:33 -0600

    pivot update

--- TransferLearning_GitHub/src/tf/autoodc/pivot/Pivot.java
+++ TransferLearning_GitHub/src/tf/autoodc/pivot/Pivot.java
@@ -1,13 +1,21 @@
 package tf.autoodc.pivot;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class Pivot {
 	static int window_size = 3;
+	ArrayList<String> pivots = new ArrayList<String>();
+	Map<String, String[]> alltext;
+	public Pivot(Map<String, String[]> alltext){
+		this.alltext = alltext;
+	}
 	
-	public void getPivotFeature(Map<String, String[]> alltext, Map<String[], String> keywords_map){
-		for(String text : alltext.keySet()){
-			
+	public void getPivotFeature(Map<String[], String> keywords_map){
+		for(String keys[] : keywords_map.keySet()){
+			for(String key : keys){
+				pivots.add(key); // add each key word to pivot arraylist as base feature vector
+			}
 		}
 	}
 }
