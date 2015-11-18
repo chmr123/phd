13894478ed9469639dccda0ac013fca6c55834f1
mingrui@Expressway Tower <mingruic@smu.edu>
2015-11-17 21:33:44 -0600

    update pivots

--- TransferLearning_GitHub/src/tf/autoodc/pivot/Documents.java
+++ TransferLearning_GitHub/src/tf/autoodc/pivot/Documents.java
@@ -6,60 +6,131 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 import com.opencsv.CSVReader;
 
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import edu.stanford.nlp.util.CoreMap;
 
 public class Documents {
-	
+
 	Stemmer stemmer = new Stemmer();
+
 	public LinkedHashMap<String, ArrayList<String>> getTokenAndPOSTag() {
 		LinkedHashMap<String, ArrayList<String>> token_type = new LinkedHashMap<String, ArrayList<String>>();
 		return token_type;
 	}
 
-	public String getPOSTag(String text,MaxentTagger tagger) {
+	public String getPOSTag(String text, MaxentTagger tagger) {
 
 		String tagged = tagger.tagString(text);
 		return tagged;
 	}
 
 	public Map<String, String[]> getTextFromFile(String[] files) throws IOException {
-	
 		Map<String, String[]> alltext = new HashMap<String, String[]>();
 		for (String filename : files) {
-			//System.out.println("Working on dataset " + filename);
+			// System.out.println("Working on dataset " + filename);
 			CSVReader reader = new CSVReader(new FileReader(filename));
-			
+
 			String[] nextLine;
-			
+
 			while ((nextLine = reader.readNext()) != null) {
 				String line = nextLine[0].toLowerCase() + " " + nextLine[1].toLowerCase();
-				String[] split = line.split("\\s+");
-				String line_after_stemmed = "";
-				for(String s : split){
-					line_after_stemmed = line_after_stemmed + stem(s) + " ";
-				}
+
+				// No stem at this time
+				/*
+				 * String[] split = line.split("\\s+"); String
+				 * line_after_stemmed = ""; for(String s : split){
+				 * line_after_stemmed = line_after_stemmed + stem(s) + " "; }
+				 */
 				String[] category_file = new String[2];
 				category_file[0] = nextLine[2];
 				category_file[1] = filename;
-				alltext.put(line_after_stemmed, category_file);
+				alltext.put(line, category_file);
 			}
 			reader.close();
 		}
-		
+
 		return alltext;
 	}
-	
-	private String stem(String term){
-		char[] chars = term.toCharArray();
-		for (char c : chars)
-			stemmer.add(c);
-		stemmer.stem();
-		String stemmed = stemmer.toString();
-		return stemmed;
+
+	public Map<String, Set<String>> getTextAndKeywordsFromFile(String[] files) throws IOException {
+		Map<String, Set<String>> alltext = new HashMap<String, Set<String>>();
+		for (String filename : files) {
+			// System.out.println("Working on dataset " + filename);
+			CSVReader reader = new CSVReader(new FileReader(filename));
+
+			String[] nextLine;
+
+			while ((nextLine = reader.readNext()) != null) {
+				String line = nextLine[0].toLowerCase() + " " + nextLine[1].toLowerCase();
+
+				// No stem at this time
+				/*
+				 * String[] split = line.split("\\s+"); String
+				 * line_after_stemmed = ""; for(String s : split){
+				 * line_after_stemmed = line_after_stemmed + stem(s) + " "; }
+				 */
+				Set<String> splittedKeywords = new HashSet<String>();
+				String[] keywords = nextLine[2].split(";");
+				for (String term : keywords) {
+					String[] split = term.split("\\s+");
+					for (String s : split) {
+						splittedKeywords.add(s);
+					}
+				}
+				alltext.put(line, splittedKeywords);
+			}
+			reader.close();
+		}
+
+		return alltext;
 	}
+
+	private String lemmatize(String text) {
+		// creates a StanfordCoreNLP object, with POS tagging, lemmatization,
+		// NER, parsing, and coreference resolution
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		// props.setProperty("annotators", "lemma");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+		// create an empty Annotation just with the given text
+		Annotation document = new Annotation(text);
+
+		// run all Annotators on this text
+		pipeline.annotate(document);
+
+		// these are all the sentences in this document
+		// a CoreMap is essentially a Map that uses class objects as keys and
+		// has values with custom types
+		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+		String lemma = null;
+		for (CoreMap sentence : sentences) {
+			// traversing the words in the current sentence
+			// a CoreLabel is a CoreMap with additional token-specific methods
+			for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+				lemma = token.get(LemmaAnnotation.class);	
+			}
+		}
+		return lemma;
+	}
+	/*
+	 * private String stem(String term){ char[] chars = term.toCharArray(); for
+	 * (char c : chars) stemmer.add(c); stemmer.stem(); String stemmed =
+	 * stemmer.toString(); return stemmed; }
+	 */
 }
--- TransferLearning_GitHub/src/tf/autoodc/pivot/Pivot.java
+++ TransferLearning_GitHub/src/tf/autoodc/pivot/Pivot.java
@@ -1,21 +1,140 @@
 package tf.autoodc.pivot;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import edu.stanford.nlp.trees.GrammaticalStructure;
+import edu.stanford.nlp.trees.GrammaticalStructureFactory;
+import edu.stanford.nlp.trees.PennTreebankLanguagePack;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreebankLanguagePack;
+import edu.stanford.nlp.trees.TypedDependency;
+import edu.stanford.nlp.util.CoreMap;
 
 public class Pivot {
 	static int window_size = 3;
-	ArrayList<String> pivots = new ArrayList<String>();
-	Map<String, String[]> alltext;
-	public Pivot(Map<String, String[]> alltext){
-		this.alltext = alltext;
+	Set<String> pivots = new HashSet<String>();
+	Map<String, Set<String>> alltextKeyWords;
+	Map<String, String[]> alltextOriginal;
+
+	public Pivot(Map<String, Set<String>> alltextKeyWords, Map<String, String[]> alltextOriginal) {
+		this.alltextKeyWords = alltextKeyWords;
+		this.alltextOriginal = alltextOriginal;
 	}
 
-	public void getPivotFeature(Map<String[], String> keywords_map){
-		for(String keys[] : keywords_map.keySet()){
-			for(String key : keys){
-				pivots.add(key); // add each key word to pivot arraylist as base feature vector
+	public void getPivotFeature(Map<String[], String> keywords_map) {
+		for (String keys[] : keywords_map.keySet()) {
+			for (String key : keys) {
+				pivots.add(key); // add each key word to pivot ArrayList as base
+									// feature vector
 			}
 		}
+	}
+
+	public Map<String, ArrayList<String>> extendFeatureSpace() {
+		Map<String, ArrayList<String>> keywords_extension = new LinkedHashMap<String, ArrayList<String>>();
+			for (String line : alltextKeyWords.keySet()) {
+				ArrayList<String> alltuples = new ArrayList<String>();
+				Set<String> keywords = alltextKeyWords.get(line);
+				for(String keyword : keywords){
+					ArrayList<String> tuples = new ArrayList<String>();
+					tuples = getKeyWordsDependency(line, keyword);
+					alltuples.addAll(tuples);
+				}
+				keywords_extension.put(line, alltuples);
+			}
+		return keywords_extension;
+	}
+
+	public Map<String, String[]> getSentenceWithPosTags(){
+		Map<String, String[]> taggedSentenceMap = new LinkedHashMap<String, String[]>();
+		for(String sentence : alltextOriginal.keySet()){
+			String tagged = postag(sentence);
+			String[] entry = alltextOriginal.get(sentence);
+			taggedSentenceMap.put(tagged, entry);
+		}
+		return taggedSentenceMap;
+	}
+	public ArrayList<String> getKeyWordsDependency(String sentence, String keyword) {
+		LexicalizedParser lp = LexicalizedParser.loadModel("/home/mingrui/Desktop/englishPCFG.ser.gz", "-maxLength",
+				"80", "-retainTmpSubcategories");
+		TreebankLanguagePack tlp = new PennTreebankLanguagePack();
+		// Uncomment the following line to obtain original Stanford Dependencies
+		// tlp.setGenerateOriginalDependencies(true);
+		GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
+		String[] array = sentence.split("\\s+");
+		Tree parse = lp.apply(Sentence.toWordList(array));
+		GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
+		Collection<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
+		ArrayList<String> keywordsDependency = new ArrayList<String>();
+
+		for (TypedDependency t : tdl) {
+			String d = t.toString();
+			String dependencyType = d.substring(0, d.indexOf("("));
+			String pair = d.substring(d.indexOf("(") + 1, d.indexOf("("));
+			String[] terms = pair.split(",");
+			String term1 = terms[0].trim();
+			String term2 = terms[1].trim();
+			if (term1.equals(keyword)) {
+				keywordsDependency.add(t.toString());
+			}
+			if (term2.equals(keyword)) {
+				keywordsDependency.add(t.toString());
+			}
+		}
+		return keywordsDependency;
+	}
+	
+	private String postag(String sentence){
+		MaxentTagger tagger = new MaxentTagger("taggers/left3words-distsim-wsj-0-18.tagger");
+        // The tagged string
+        String tagged = tagger.tagString(sentence);
+        // Output the result
+        return tagged;
+	}
+	
+	private String lemmatize(String text) {
+		// creates a StanfordCoreNLP object, with POS tagging, lemmatization,
+		// NER, parsing, and coreference resolution
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		// props.setProperty("annotators", "lemma");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+		// create an empty Annotation just with the given text
+		Annotation document = new Annotation(text);
+
+		// run all Annotators on this text
+		pipeline.annotate(document);
+
+		// these are all the sentences in this document
+		// a CoreMap is essentially a Map that uses class objects as keys and
+		// has values with custom types
+		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+		String lemma = null;
+		for (CoreMap sentence : sentences) {
+			// traversing the words in the current sentence
+			// a CoreLabel is a CoreMap with additional token-specific methods
+			for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+				lemma = token.get(LemmaAnnotation.class);	
+			}
+		}
+		return lemma;
 	}
 }
